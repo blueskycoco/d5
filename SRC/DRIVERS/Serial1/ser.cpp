f7deb9390f9d6fef0005781a7364d8fe367c0c99
pc home <pc_home@gmail.com>
2014-08-21 22:19:37 +0800

    remove uart3 log

--- SRC/DRIVERS/Serial1/ser.cpp
+++ SRC/DRIVERS/Serial1/ser.cpp
@@ -50,7 +50,7 @@ public:
 		    ddi.dwSysintr = 0;
 		    return FALSE;
 		}else
-			RETAILMSG(1, (TEXT("OK: UART1: request the UART1 sysintr. %x\r\n"),ddi.dwSysintr));	
+			RETAILMSG(0, (TEXT("OK: UART1: request the UART1 sysintr. %x\r\n"),ddi.dwSysintr));	
             RegSetValueEx(DEVLOAD_SYSINTR_VALNAME,REG_DWORD,(PBYTE)&ddi.dwSysintr, sizeof(UINT32));
             
             return CPdd3250Uart::Init();
@@ -63,7 +63,7 @@ public:
 CSerialPDD * CreateSerialObject(LPTSTR lpActivePath, PVOID pMdd,PHWOBJ pHwObj, DWORD DeviceArrayIndex)
 {
     CSerialPDD * pSerialPDD = NULL;
-    RETAILMSG( TRUE, (TEXT("DEBUG: CreateSerialObject %d\r\n"), DeviceArrayIndex)); 
+    RETAILMSG(0, (TEXT("DEBUG: CreateSerialObject %d\r\n"), DeviceArrayIndex)); 
     switch (DeviceArrayIndex) {
       case 1:        ///< UART1
         pSerialPDD = new CPdd3250Serial1(lpActivePath,pMdd, pHwObj);
